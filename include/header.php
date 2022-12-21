ea59d2b09bb0eaa10866f520dbb4a1f7ba317fc7
muhsin16290058 <delpiero1907@outlook.com>
2022-12-21 23:13:52 +0300

    created home page

---
+++ include/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title></title>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script> 
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg navbar-info bg-info">
+        <h5 class="text-white">Hospital Management System</h5>
+
+        <div style="margin-left: auto" class="mr-auto"></div>
+
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Nurse</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>
+            
+        </ul>
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+
+    <link rel="stylesheet" href="index.css">
+</head>
+<body class="Main">
+
+	<?php
+
+		include("include/header.php");
+	?>
+	<div style="margin-top: 40px;"></div>
+	<div class="container">
+	<div class="col-md-12">
+		<div class="row">
+			<div div="info" class="col-md-3 mx-1 shadow" style="text-align: center;" >
+
+				<img src="images/info.png" style="width: 50%; height: 50%";>
+
+				<h5 class="text-center">Click on the button for more information</h5>
+
+				<a href="#">
+					<button class="button">More Info</button>
+				</a>
+				
+			</div>
+			<div class="col-md-3 mx-1 shadow" style="text-align: center;">
+				
+				<img src="images/homePatient.jpg" style="width: 50%; height: 50%;">
+
+				<h5 class="text-center">Create account so that we can take good care of you</h5>
+ 
+				<a href="#">
+					<button class="button">Create Account!</button>
+				</a>
+			</div>
+			<div class="col-md-3 mx-1 shadow" style="text-align: center;">
+				
+				<img src="images/contracted_institutions.jpg" style="width: 50%; height: 50%;">
+
+				<h5 class="text-center">Contracted Institutions</h5>
+ 
+				<a href="#">
+					<button class="button" >Take a quick look!</button>
+				</a>
+			</div>
+			<div class="col-md-3 mx-1 shadow" style="text-align: center;">
+				
+				<img src="images/homeNurse.jpg" style="width: 50%; height: 50%;">
+
+				<h5 class="text-center">We are looking for nurses who love their job.</h5>
+ 
+				<a href="#">
+					<button class="button" >Apply Now!</button>
+				</a>
+			</div>
+			<div class="col-md-3 mx-1 shadow" style="text-align: center;">
+				
+				<img src="images/dr.png" style="width: 50%; height: 50%;">
+
+				<h5 class="text-center">We are employing high-quality doctors</h5>
+
+				
+				<a href="#">
+					<button class="button">Apply Now!</button>
+				</a>
+				
+			</div>
+		</div>
+	</div>
+	</div>
+
+</body>
+</html>
